e3f09d2d7f8b00f9e0eb94b4c3316100a0adf988
muhfathoni <muh.fathoni@gmail.com>
2019-03-05 19:42:15 +0700

    "update booking service 2"

--- app/src/main/java/com/example/sibengkel/BookingService.java
+++ app/src/main/java/com/example/sibengkel/BookingService.java
@@ -29,7 +29,7 @@ public class BookingService extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);
+        setContentView(R.layout.activity_booking_service);
 
         initiateCoreApp();
 
