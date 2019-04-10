e42067e7c8c08b4b4611174d57bcf067d6f8a48e
faganesha <ganeshafadel@gmail.com>
2019-04-10 23:09:49 +0700

    navbar

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -76,17 +76,17 @@ public final class R {
         public static final int bottom = 0x7f080023;
         public static final int end = 0x7f080048;
         public static final int gone = 0x7f080056;
-        public static final int invisible = 0x7f080063;
-        public static final int left = 0x7f080068;
-        public static final int packed = 0x7f080082;
-        public static final int parent = 0x7f080084;
-        public static final int percent = 0x7f080089;
-        public static final int right = 0x7f080090;
-        public static final int spread = 0x7f0800b5;
-        public static final int spread_inside = 0x7f0800b6;
-        public static final int start = 0x7f0800bb;
-        public static final int top = 0x7f0800d0;
-        public static final int wrap = 0x7f0800e3;
+        public static final int invisible = 0x7f080064;
+        public static final int left = 0x7f080069;
+        public static final int packed = 0x7f080083;
+        public static final int parent = 0x7f080085;
+        public static final int percent = 0x7f08008a;
+        public static final int right = 0x7f080091;
+        public static final int spread = 0x7f0800b6;
+        public static final int spread_inside = 0x7f0800b7;
+        public static final int start = 0x7f0800bc;
+        public static final int top = 0x7f0800d1;
+        public static final int wrap = 0x7f0800e4;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/sibengkel/MainActivity.java
+++ app/src/main/java/com/example/sibengkel/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.sibengkel;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -32,7 +36,35 @@ public class MainActivity extends AppCompatActivity {
                 logout();
             }
         });
-    }
+
+//        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+//        bottomNav.setOnNavigationItemSelectedListener(navListener);
+//
+//        if(savedInstanceState==null){
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+//        }
+//    }
+//
+//    private BottomNavigationView.OnNavigationItemSelectedListener navListener =  new BottomNavigationView.OnNavigationItemSelectedListener() {
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//            Fragment selectedFragment = null;
+//
+//            switch (menuItem.getItemId()) {
+//                case R.id.nav_home:
+//                    selectedFragment = new HomeFragment();
+//                    break;
+//                case R.id.nav_profile:
+//                    selectedFragment = new SettingFragment();
+//                    break;
+//            }
+//
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                    selectedFragment).commit();
+//
+//            return true;
+//        }
+    };
 
     public void bookNow(View view){
         intent = new Intent(MainActivity.this, ScheduleActivity.class);
